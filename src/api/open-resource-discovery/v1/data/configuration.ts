0dbbaeef81695ba17ea566f2291b8aa37e08b2ea
Simon Heimler <simon.heimler@sap.com>
2025-05-16 15:05:47 +0200

    Add back perspective on ORD config level
    Signed-off-by: Simon Heimler <simon.heimler@sap.com>

--- src/api/open-resource-discovery/v1/data/configuration.ts
+++ src/api/open-resource-discovery/v1/data/configuration.ts
@@ -7,12 +7,15 @@ export const ordConfiguration: ORDConfiguration = {
       // Serve static metadata with open access strategy, ignore tenant headers
       {
         url: '/open-resource-discovery/v1/documents/system-version',
-        accessStrategies: [openAccessStrategy]
+        accessStrategies: [openAccessStrategy],
+        perspective: "system-version"
       },
       // Serve dynamic metadata, requires system / tenant headers and the correct access strategy
       {
         url: '/open-resource-discovery/v1/documents/system-instance',
         accessStrategies: [customAccessStrategyGlobalTenantId, customAccessStrategyLocalTenantId],
+        perspective: "system-instance"
+
       },
     ],
   },
